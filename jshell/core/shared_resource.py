94d42912e2fc09f07d4c11e66817f5f53e0a7e8a
Jean-Loutre <jean-loutre@gmx.com>
2023-04-10 20:04:58 +0200

    feat: add shared resource

---
+++ jshell/core/shared_resource.py
@@ -0,0 +1,39 @@
+"""Shared resource allow to share code between tasks."""
+from asyncio import Lock
+from types import TracebackType
+from typing import AsyncContextManager, Callable, Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class SharedResource(Generic[T]):
+    def __init__(self, loader: Callable[[], AsyncContextManager[T]]) -> None:
+        self._loader = loader
+        self._context_manager: AsyncContextManager[T] | None = None
+        self._resource: T | None = None
+        self._lock = Lock()
+        self._acquire_count = 0
+
+    async def __aenter__(self) -> T:
+        async with self._lock:
+            if self._context_manager is None:
+                assert self._resource is None
+                self._context_manager = self._loader()
+                self._resource = await self._context_manager.__aenter__()
+            self._acquire_count = self._acquire_count + 1
+        assert self._resource is not None
+        return self._resource
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        async with self._lock:
+            self._acquire_count = self._acquire_count - 1
+            if self._acquire_count == 0:
+                assert self._context_manager is not None
+                await self._context_manager.__aexit__(exc_type, exc_val, exc_tb)
+                self._context_manager = None
+                self._resource = None
---
+++ tests/core/test_shared_resource.py
@@ -0,0 +1,33 @@
+"""Config unit tests."""
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+from unittest.mock import Mock
+
+from jshell.core.shared_resource import SharedResource
+
+
+async def test_acquire_release() -> None:
+    """load_from_python should load a class from a python file."""
+    load = Mock()
+    cleanup = Mock()
+
+    @asynccontextmanager
+    async def load_otter() -> AsyncIterator[object]:
+        load()
+        yield object()
+        cleanup()
+
+    otter_resource = SharedResource(load_otter)
+
+    async with otter_resource as otter_1:
+        load.assert_called_once()
+        cleanup.assert_not_called()
+
+        async with otter_resource as otter_2:
+            load.assert_called_once()
+            assert otter_1 == otter_2
+            cleanup.assert_not_called()
+
+        cleanup.assert_not_called()
+
+    cleanup.assert_called_once()
