bf38638979871acf93292fb2f05397c1027b0d70
Jean-Loutre <jean-loutre@gmx.com>
2023-04-25 13:00:12 +0200

    refactor: factorize manifest test method

--- tests/systems/test_os.py
+++ tests/systems/test_os.py
@@ -27,100 +27,89 @@ class MockOs(Os):
         await self._sh(f"permissions {path} {user} {group} {mode}")
 
 
-async def test_manifest_content_string() -> None:
+async def _check_manifest(manifest: str, *processes: MockProcess) -> None:
     async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("write /etc/otters", expected_stdin=b"Kweek kweek\n")
-
-    manifest = """
-    files:
-      /etc/otters:
-        content: |
-          Kweek kweek
-    """
+        for process in processes:
+            yield process
 
     async with MockShell(_system_mock()) as sh:
         os = MockOs(sh)
         await os.sync_manifest(manifest)
+
+
+async def test_manifest_content_string() -> None:
+    await _check_manifest(
+        """
+        files:
+          /etc/otters:
+            content: |
+              Kweek kweek
+        """,
+        check_process("write /etc/otters", expected_stdin=b"Kweek kweek\n"),
+    )
 
 
 async def test_manifest_content_base64() -> None:
-    async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("write /etc/otters", expected_stdin=b"Kweek kweek\n")
-
-    manifest = """
-    files:
-      /etc/otters:
-        content: !base64 S3dlZWsga3dlZWsK
-    """
-
-    async with MockShell(_system_mock()) as sh:
-        os = MockOs(sh)
-        await os.sync_manifest(manifest)
+    await _check_manifest(
+        """
+        files:
+          /etc/otters:
+            content: !base64 S3dlZWsga3dlZWsK
+        """,
+        check_process("write /etc/otters", expected_stdin=b"Kweek kweek\n"),
+    )
 
 
 async def test_manifest_content_directory() -> None:
-    async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("mkdir /etc/otters")
-        yield check_process("permissions /etc/otters/peter_file peter None None")
-        yield check_process("permissions /etc/otters/steven_file steven None None")
-
-    manifest = """
-    files:
-      /etc/otters:
-        content: !dir
-          peter_file:
-            user: peter
-          steven_file:
-            user: steven
-    """
-    async with MockShell(_system_mock()) as sh:
-        os = MockOs(sh)
-        await os.sync_manifest(manifest)
+    await _check_manifest(
+        """
+        files:
+          /etc/otters:
+            content: !dir
+              peter_file:
+                user: peter
+              steven_file:
+                user: steven
+        """,
+        check_process("mkdir /etc/otters"),
+        check_process("permissions /etc/otters/peter_file peter None None"),
+        check_process("permissions /etc/otters/steven_file steven None None"),
+    )
 
 
 async def test_manifest_link_content() -> None:
-    async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("link /peter /steven")
-
-    manifest = """
-    files:
-      /steven:
-        content: !link /peter
-    """
-
-    async with MockShell(_system_mock()) as sh:
-        os = MockOs(sh)
-        await os.sync_manifest(manifest)
+    await _check_manifest(
+        """
+        files:
+          /steven:
+            content: !link /peter
+        """,
+        check_process("link /peter /steven"),
+    )
 
 
 async def test_manifest_file_mode() -> None:
-    async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("permissions /etc/otters peter peter 0755")
-
-    manifest = """
-    files:
-      /etc/otters:
-        user: peter
-        group: peter
-        mode: 0755
-    """
-    async with MockShell(_system_mock()) as sh:
-        os = MockOs(sh)
-        await os.sync_manifest(manifest)
+    await _check_manifest(
+        """
+        files:
+          /etc/otters:
+            user: peter
+            group: peter
+            mode: 0755
+        """,
+        check_process("permissions /etc/otters peter peter 0755"),
+    )
 
 
 async def test_manifest_multiple_target() -> None:
-    async def _system_mock() -> AsyncIterator[MockProcess]:
-        yield check_process("permissions /etc/wubba steven otters 0755")
-        yield check_process("permissions /etc/lubba steven otters 0755")
-
-    manifest = """
-    files:
-      [/etc/wubba, /etc/lubba]:
-        user: steven
-        group: otters
-        mode: 0755
-    """
-    async with MockShell(_system_mock()) as sh:
-        os = MockOs(sh)
-        await os.sync_manifest(manifest)
+    await _check_manifest(
+        """
+        files:
+          [/etc/wubba, /etc/lubba]:
+            user: steven
+            group: otters
+            mode: 0755
+        """,
+        check_process("permissions /etc/wubba steven otters 0755"),
+        check_process("permissions /etc/lubba steven otters 0755"),
+    )
