7967a6cf746b710e5c0f255c93d6c79cdd1b1677
Corentin Séchet <corentin@ki-dour.org>
2024-02-16 14:24:34 +0100

    feat: add ssh shell

---
+++ jiac/systems/ssh.py
@@ -0,0 +1,69 @@
+"""Connectors are usable to send commands to a target."""
+from logging import Logger
+from typing import Any, AsyncIterator
+from contextlib import asynccontextmanager
+
+from asyncssh import (
+    SSHClientConnection,
+    SSHClientConnectionOptions,
+    SSHClientProcess,
+    SSHWriter,
+    connect,
+)
+from asyncssh.logging import SSHLogger
+
+from jiac.shell import Process, Shell, Stderr, Stdout
+from jiac.stream import Stream
+
+
+@asynccontextmanager
+async def ssh_shell(
+    host: str, logger: Logger | None = None, **kwargs: Any
+) -> AsyncIterator[Shell]:
+    async with connect(
+        host, options=SSHClientConnectionOptions(**kwargs)
+    ) as connection:
+        yield _SshShell(connection, logger=logger)
+
+
+class _SshStream(Stream):
+    def __init__(self, writer: SSHWriter[bytes]):
+        self._writer = writer
+
+    async def write(self, data: bytes) -> None:
+        self._writer.write(data)
+        await self._writer.drain()
+
+    async def close(self) -> None:
+        self._writer.write_eof()
+        await self._writer.drain()
+        await self._writer.wait_closed()
+
+
+class _SshShell(Shell):
+    def __init__(
+        self, connection: SSHClientConnection, logger: Logger | None = None
+    ) -> None:
+        super().__init__(logger=logger)
+        self._connection = connection
+        if logger:
+            ssh_logger = logger.getChild("ssh")
+            self._connection._logger = SSHLogger(parent=ssh_logger)
+
+    async def _start_process(
+        self, out: Stdout, err: Stderr, command: str, env: dict[str, str]
+    ) -> Process:
+        process: SSHClientProcess[Any] = await self._connection.create_process(
+            command, stdout=out, stderr=err, env=env, encoding=None
+        )
+
+        async def _wait() -> int:
+            await process.wait_closed()
+            return_code = process.returncode
+            if return_code is None:
+                # TODO: Handle this better, None value is when process
+                # exits due to a signal.
+                return -1
+            return return_code
+
+        return _SshStream(process.stdin), err, _wait()
---
+++ tests/systems/test_ssh.py
@@ -0,0 +1,54 @@
+"""Config unit tests."""
+from unittest.mock import ANY, AsyncMock, patch
+
+from asyncssh import SSHClientConnectionOptions
+
+from jiac.shell import Stderr, Stdout
+from jiac.systems.ssh import ssh_shell
+
+
+@patch("jiac.systems.ssh.connect")
+async def test_connect(connect_mock: AsyncMock) -> None:
+    async with ssh_shell(host="otters.org", username="gilbert"):
+        pass
+
+    connect_mock.return_value.__aenter__.assert_awaited_once()
+    connect_mock.return_value.__aexit__.assert_awaited_once()
+
+    connect_mock.assert_called_once()
+    args, kwargs = connect_mock.call_args_list[0]
+    assert args == ("otters.org",)
+
+    options = kwargs["options"]
+    assert isinstance(options, SSHClientConnectionOptions)
+    assert options.username == "gilbert"
+
+
+@patch("jiac.systems.ssh.connect")
+async def test_run(connect_mock: AsyncMock) -> None:
+    async with ssh_shell("otters.org", username="gilbert") as sh:
+
+        async def create_process(
+            _: str,
+            stdout: Stdout,
+            stderr: Stderr,
+            env: dict[str, str],  # pylint: disable=unused-argument
+            encoding: str,  # pylint: disable=unused-argument
+        ) -> AsyncMock:
+            if stdout:
+                await stdout.close()
+            if stderr:
+                await stderr.close()
+            process_mock = AsyncMock()
+            process_mock.returncode = 0
+            return process_mock
+
+        create_process_mock = (
+            connect_mock.return_value.__aenter__.return_value.create_process
+        )
+        create_process_mock.side_effect = create_process
+
+        await sh("tickle otter")
+        create_process_mock.assert_awaited_once_with(
+            "tickle otter", stdout=ANY, stderr=ANY, env={}, encoding=None
+        )
