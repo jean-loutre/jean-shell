ea7aae775041a3ac99959c577949a806741a1e92
Corentin Séchet <corentin@ki-dour.org>
2024-02-16 14:29:07 +0100

    feat: add local shell

---
+++ jiac/systems/local.py
@@ -0,0 +1,66 @@
+from asyncio import StreamReader, StreamWriter, create_subprocess_shell, create_task
+from logging import Logger
+from subprocess import PIPE
+
+from jiac.shell import Process, Shell, Stderr, Stdout
+from jiac.stream import Stream
+
+
+class LocalShell(Shell):
+    def __init__(
+        self, logger: Logger | None = None, raise_on_error: bool = False
+    ) -> None:
+        super().__init__(logger=logger, raise_on_error=raise_on_error)
+
+    async def _start_process(
+        self, out: Stdout, err: Stderr, command: str, env: dict[str, str]
+    ) -> Process:
+        process = await create_subprocess_shell(
+            command,
+            stdout=PIPE,
+            stderr=PIPE,
+            stdin=PIPE,
+            env=env,
+        )
+
+        async def _pipe(src: StreamReader, dst: Stdout | Stderr) -> None:
+            if dst is None:
+                return
+            while True:
+                data = await src.read(1024)
+                if not data:
+                    break
+                await dst.write(data)
+
+        stdout = process.stdout
+        assert stdout is not None
+
+        stderr = process.stderr
+        assert stderr is not None
+
+        stdin = process.stdin
+        assert stdin is not None
+
+        pipe_stdout = create_task(_pipe(stdout, out))
+        pipe_stderr = create_task(_pipe(stderr, err))
+
+        async def _wait() -> int:
+            return_code = await process.wait()
+            await pipe_stdout
+            await pipe_stderr
+            return return_code
+
+        return _ProcessStream(stdin), err, _wait()
+
+
+class _ProcessStream(Stream):
+    def __init__(self, writer: StreamWriter) -> None:
+        self._writer = writer
+
+    async def write(self, data: bytes) -> None:
+        self._writer.write(data)
+        await self._writer.drain()
+
+    async def close(self) -> None:
+        self._writer.close()
+        await self._writer.wait_closed()
---
+++ tests/systems/test_local.py
@@ -0,0 +1,137 @@
+"""Config unit tests."""
+from asyncio import StreamReader
+from contextlib import contextmanager
+from subprocess import PIPE
+from typing import Iterator
+from unittest.mock import AsyncMock, patch
+
+from jiac.shell import FROM_STDERR, FROM_STDOUT, echo, redirect
+from jiac.systems.local import LocalShell
+
+
+class _StreamWriterMock:
+    def __init__(self) -> None:
+        self._data = b""
+
+    @property
+    def data(self) -> bytes:
+        return self._data
+
+    def write(self, data: bytes) -> None:
+        self._data = self._data + data
+
+    async def drain(self) -> None:
+        pass
+
+    def close(self) -> None:
+        pass
+
+    async def wait_closed(self) -> None:
+        pass
+
+
+class _ProcessMock:
+    def __init__(
+        self,
+        stdin: _StreamWriterMock,
+        stdout_data: bytes,
+        stderr_data: bytes,
+        return_code: int = 0,
+    ) -> None:
+        self._stdout = StreamReader()
+        self._stdout.feed_data(stdout_data)
+        self._stdout.feed_eof()
+
+        self._stderr = StreamReader()
+        self._stderr.feed_data(stderr_data)
+        self._stderr.feed_eof()
+
+        self._stdin = stdin
+        self._return_code = return_code
+
+    @property
+    def stdout(self) -> StreamReader:
+        return self._stdout
+
+    @property
+    def stderr(self) -> StreamReader:
+        return self._stderr
+
+    @property
+    def stdin(self) -> _StreamWriterMock:
+        return self._stdin
+
+    async def wait(self) -> int:
+        return self._return_code
+
+
+@contextmanager
+def mock_create_process(
+    stdout_data: bytes | None = None,
+    stderr_data: bytes | None = None,
+    stdin: _StreamWriterMock | None = None,
+    return_code: int = 0,
+) -> Iterator[AsyncMock]:
+    stdout_data = stdout_data or b""
+    stderr_data = stderr_data or b""
+    stdin = stdin or _StreamWriterMock()
+    process_mock = _ProcessMock(stdin, stdout_data, stderr_data, return_code)
+
+    async def create_process(
+        command: str,
+        stdout: int,
+        stderr: int,
+        env: dict[str, str],  # pylint: disable=unused-argument
+    ) -> _ProcessMock:
+        assert command == "tickle otter"
+        assert stdout == PIPE
+        assert stderr == PIPE
+        return process_mock
+
+    with patch(
+        "jiac.systems.local.create_subprocess_shell",
+        return_value=process_mock,
+        autospec=True,
+    ) as mock:
+        yield mock
+
+
+async def test_run() -> None:
+    with mock_create_process(return_code=1) as mock:
+        sh = LocalShell()
+        with sh.raise_on_error(False):
+            with sh.env(OTTER_SENSITIVITY="low"):
+                assert (await sh("tickle otter")) == 1
+                mock.assert_awaited_once_with(
+                    "tickle otter",
+                    stdout=PIPE,
+                    stderr=PIPE,
+                    stdin=PIPE,
+                    env={"OTTER_SENSITIVITY": "low"},
+                )
+
+
+async def test_stdout() -> None:
+    with mock_create_process(stdout_data=b"kweek kweek"):
+        sh = LocalShell()
+        out = bytearray()
+        await (sh("tickle otter") >> out)
+        assert out == b"kweek kweek"
+
+
+async def test_stderr() -> None:
+    with mock_create_process(stderr_data=b"kweek kweek"):
+        sh = LocalShell()
+        out = bytearray()
+        await (
+            sh("tickle otter") | redirect(stdout=FROM_STDERR, stderr=FROM_STDOUT) >> out
+        )
+        assert out == b"kweek kweek"
+
+
+async def test_stdin() -> None:
+    """LocalShell should write to subprocess stdin."""
+    with mock_create_process(stderr_data=b"kweek kweek") as mock:
+        sh = LocalShell()
+        await (echo(b"kweek kweek") | sh("tickle otter"))
+        assert mock.return_value.stdin.data == b"kweek kweek"
